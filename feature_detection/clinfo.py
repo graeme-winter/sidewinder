b67f68bcc4b88fba9243755dd47a3982177bb4df
Graeme Winter <graeme.winter@gmail.com>
2021-04-21 11:09:55 +0100

    Clinfo

---
+++ feature_detection/clinfo.py
@@ -0,0 +1,33 @@
+import sys
+import time
+import math
+
+import numpy as np
+import pyopencl as cl
+
+from __init__ import setup, mask, data, shape, rettilb
+
+
+def get_devices():
+    result = []
+    for pl in cl.get_platforms():
+        result.extend(pl.get_devices())
+    return result
+
+
+def clinfo():
+    devices = get_devices()
+    print("Available devices:")
+    for j, dev in enumerate(devices):
+        print(f"{j} -> {dev.name}")
+        print(f"Type:                 {cl.device_type.to_string(dev.type)}")
+        print(f"Vendor:               {dev.vendor}")
+        print(f"Available:            {bool(dev.available)}")
+        print(f"Memory (global / MB): {int(dev.global_mem_size/1024**2)}")
+        print(f"Memory (local / B):   {dev.local_mem_size}")
+        print(f"Max work group:       {dev.max_work_group_size}")
+        print(f"Max work items:       {dev.max_work_item_sizes}")
+        print("")
+
+
+clinfo()
